69a3f32162738f3c15f3d93bc4877ad7720465a2
hakolr <m.gayvoronskiy@flang.io>
2026-08-05 09:58:59 +0300

    chore: dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,11 +9,11 @@ const recapEngine = path.resolve(root, '../recap-engine/packages');
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@recap-engine/core': path.join(recapEngine, 'core/src/index.ts'),
-      '@recap-engine/react/styles.css': path.join(recapEngine, 'react/src/styles.css'),
-      '@recap-engine/react': path.join(recapEngine, 'react/src/index.ts'),
-    },
+    // alias: {
+    //   '@recap-engine/core': path.join(recapEngine, 'core/src/index.ts'),
+    //   '@recap-engine/react/styles.css': path.join(recapEngine, 'react/src/styles.css'),
+    //   '@recap-engine/react': path.join(recapEngine, 'react/src/index.ts'),
+    // },
     dedupe: ['react', 'react-dom'],
   },
   server: {
